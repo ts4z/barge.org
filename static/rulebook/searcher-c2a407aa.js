84c59592df1bbf1a5d658dfeb94a418e9ff40edb
ts4z <>
2026-09-20 18:35:08 +0000

    Update from https://github.com/ts4z/barge-rulebook/commit/0b65519405c65a3d0017cac1522d0f815449ba9b

--- static/rulebook/searcher-c2a407aa.js
+++ static/rulebook/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-718ab2da.js',
+                path_to_root + 'searchindex-3df7de13.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
